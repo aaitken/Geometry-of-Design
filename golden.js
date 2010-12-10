688a7b29d70740fc384c52ec2bff597390958fcb
Alex Aitken <aaitken@alexaitken.net>
2010-12-09 21:27:32 -0700

    Golden Section and Root 2 triangle definitions
    paramater object properties in GOD.draw call define size and type of
    rectangle while calculations in function (which make use of Math.asine
    to derive angle) determine lines to stroke.

--- golden.js
+++ golden.js
@@ -1,32 +1,48 @@
 var GOD={}; //geometry of design namespace
 
-GOD.draw={};
-GOD.draw.rootTwo=function(sDef){
+GOD.draw=function(){
     /*
     * Define
     */
-    var canvas=document.getElementById('goldenSection'),
-        ctx=(function(){
-            if(canvas.getContext){return canvas.getContext('2d');}
+    var cnvs=arguments[0].targ,
+        ctxt=(function(){
+            if(cnvs.getContext){return cnvs.getContext('2d');}
             else{return null;}//canvas not supported
         }()),
         //re-used lookups
-        sqrX=sDef.x,
-        sqrY=sDef.y,
-        sqrW=sDef.w,
-        sqrH=sDef.h,
-        radi=Math.sqrt(2*Math.pow(sqrH,2));
+        type=arguments[0].type,
+        size=arguments[0].size,
+        radi,//arc radius = right angle hypotenuse - defined below off 'type'
+        arcX,//arc center - defined below off 'type'
+        arcL,//arc angle - defined below off 'type'
+        recW;//reciprocal rectangle width - defined below off 'type'
+        switch(type){
+            case 'r2'://root 2
+                radi=Math.sqrt(2*Math.pow(size,2));
+                arcX=0;
+                recW=radi-size;
+                break;
+            case 'gs'://golden section
+                radi=Math.sqrt(Math.pow(size,2)+Math.pow(0.5*size,2));
+                arcX=0.5*size,
+                recW=radi-0.5*size;
+        }
+        arcL=Math.asin(size/radi);//returns radian angle of sine value size/radi
     /*
     * Draw
-    */
-    if(ctx!==null){
-        canvas.setAttribute('height',sqrH);
-        canvas.setAttribute('width',radi+1);
-        ctx.strokeRect(sqrX,sqrY,sqrW,sqrH);
-        ctx.arc(sqrX,sqrH,radi,(Math.PI/180)*315,(Math.PI/180)*0,false);
-        ctx.stroke();
-        ctx.strokeRect(sqrW,sqrY,radi-sqrW,sqrH);
+    */ 
+    if(ctxt!==null){
+        cnvs.setAttribute('height',size);
+        cnvs.setAttribute('width',size+recW+2);
+        ctxt.strokeRect(0,0,size,size);
+        ctxt.arc(arcX,size,radi,arcL,0,false);
+        ctxt.stroke();
+        ctxt.strokeRect(size,0,recW,size);
     }
 };
 
-GOD.draw.rootTwo({x:0,y:0,w:150,h:150});
+GOD.draw({
+    targ:document.getElementById('goldenSection'),
+    type:'gs',
+    size:250
+});

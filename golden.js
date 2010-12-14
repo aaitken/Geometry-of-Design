c3b432806e18b6fdc0a66289e40a808bab0ecb8f
Alex Aitken <aaitken@alexaitken.net>
2010-12-13 22:59:14 -0700

    Golden Section and Root 2-5 rectangle calculations abstracted in to calc object.
    With this approach we cumulatively define root
    rectangles 3-5 based on their root 2 predecessors, per Elam.

--- golden.js
+++ golden.js
@@ -10,39 +10,77 @@ GOD.draw=function(){
             else{return null;}//canvas not supported
         }()),
         //re-used lookups
-        type=arguments[0].type,
-        size=arguments[0].size,
-        radi,//arc radius = right angle hypotenuse - defined below off 'type'
-        arcX,//arc center - defined below off 'type'
-        arcL,//arc angle - defined below off 'type'
-        recW;//reciprocal rectangle width - defined below off 'type'
-        switch(type){
-            case 'r2'://root 2
+        size=arguments[0].size+0.5,//for single pixel width
+        radi,//arc radius = right angle hypotenuse - defined below per 'type'
+        arcX,//arc center - defined below per 'type'
+        arcL,//arc angle - defined below per 'type'
+        recW,//reciprocal rectangle width - defined below per 'type',
+        recE,//reciprocal left edge
+        //calculation functions
+        calc={
+            update:function(){
+                recE=recE+recW;
+                radi=Math.sqrt(Math.pow(size,2)+Math.pow(radi,2));
+                recW=radi-recE;
+            },
+            gs:function(){
+                radi=Math.sqrt(Math.pow(size,2)+Math.pow(0.5*size,2));
+                arcX=0.5*size;
+                recW=radi-0.5*size;
+                recE=size;
+            },
+            r2:function(){//root 2
                 radi=Math.sqrt(2*Math.pow(size,2));
                 arcX=0;
                 recW=radi-size;
-                break;
-            case 'gs'://golden section
-                radi=Math.sqrt(Math.pow(size,2)+Math.pow(0.5*size,2));
-                arcX=0.5*size,
-                recW=radi-0.5*size;
-        }
+                recE=size;
+            },
+            r3:function(){
+                this.r2();
+                this.update();
+            },
+            r4:function(){
+                this.r3();
+                this.update();
+            },
+            r5:function(){
+                this.r4();
+                this.update();
+            }
+        };
+        calc[arguments[0].type]();
         arcL=Math.asin(size/radi);//returns radian angle of sine value size/radi
     /*
     * Draw
     */ 
     if(ctxt!==null){
-        cnvs.setAttribute('height',size);
-        cnvs.setAttribute('width',size+recW+2);
-        ctxt.strokeRect(0,0,size,size);
-        ctxt.arc(arcX,size,radi,arcL,0,false);
+        cnvs.setAttribute('height',800);
+        cnvs.setAttribute('width',800);
+        //root
+        ctxt.beginPath();
+        ctxt.moveTo(0.5,0.5);
+        ctxt.lineTo(0.5,size);//left
+        ctxt.lineTo(recE,size);//bottom
+        ctxt.lineTo(recE,0.5);//right
+        ctxt.lineTo(0.5,0.5);//top
         ctxt.stroke();
-        ctxt.strokeRect(size,0,recW,size);
+        //arc
+        ctxt.beginPath();
+        ctxt.arc(arcX,size,radi,0-arcL,0,false);
+        ctxt.stroke();
+        //reciprocal
+        ctxt.beginPath();
+        ctxt.moveTo(recE,0.5);
+        ctxt.moveTo(recE,size);//left
+        ctxt.lineTo(recE+recW,size);//bottom
+        ctxt.lineTo(recE+recW,0.5);//right
+        ctxt.lineTo(recE,0.5);//top
+        ctxt.stroke();
     }
 };
 
 GOD.draw({
     targ:document.getElementById('goldenSection'),
-    type:'gs',
-    size:250
+    type:'r2',
+    size:200
 });
